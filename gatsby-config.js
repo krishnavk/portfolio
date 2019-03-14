28fd287ce572925baccf4fa9a1bfd24dafd34484
Krishna <krishnavkishore@gmail.com>
2019-03-14 20:03:33 +0530

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,16 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-sass`,
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
